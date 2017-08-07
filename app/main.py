0cbf8b4dd7a9f5d8729b1d3fa8e9b0d1b8468658
Huw Fryer <hf1g10@soton.ac.uk>
2017-08-07 22:42:00 +0000

    Minor modifications and additions

--- app/main.py
+++ app/main.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return "<html><head><title>FB Experiment</title></head><body><h1>I'm Ron Burgundy?</h1></body></html>"
 
+@app.route("/ping/<hash>")
+def ping():
+    pass
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
