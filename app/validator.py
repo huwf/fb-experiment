6274dec4d0a89ea125c9ca32fa5d3b7792cbba85
Tom <t.blount@soton.ac.uk>
2017-08-18 16:59:05 +0100

    Reworked into multiple methods and try/catch to prevent hanging

--- app/validator.py
+++ app/validator.py
@@ -2,19 +2,48 @@ import time
 from datetime import datetime, timedelta
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
+MIN_TIME = timedelta(365)
+MIN_FRIENDS = 25
 
 #This requires the Firefox driver (https://github.com/mozilla/geckodriver/releases) be installed to /usr/bin
 def validate_account(email, password):
-    MIN_TIME = timedelta(365)
-    MIN_FRIENDS = 25
+    
+    
 
 
     driver = webdriver.Firefox()
     driver.get("http://www.facebook.com")
 
+    try:
+        if not log_in(driver, email, password):
+            return False
+
+        #Check for "validness" of profile here:
+        if not validate_profile_age(driver):
+            return False
+
+        #Friends check
+        if not validate_friends(driver):
+            return False
+
+        #Picture check
+        pass
+
+        #Other checks?
+        pass
+
+    except Exception as e:
+        print e
+        driver.close()
+        return False
+
+
+    driver.close()
+    return True
+
+
+def log_in(driver, email, password):
     elem = driver.find_element_by_name("email")
     elem.clear()
     elem.send_keys(email)
@@ -30,8 +59,11 @@ def validate_account(email, password):
     if "log in" in elem.text.lower():
         driver.close()
         return False # Password was wrong
+    else:
+        return True
 
-    #Check for "validness" of profile here:
+
+def validate_profile_age(driver):
     driver.find_element_by_xpath("//div[@data-click='profile_icon']").click()
     time.sleep(5)
     elem = driver.find_element_by_name("profile_id")
@@ -53,17 +85,13 @@ def validate_account(email, password):
         return False
 
     elem.send_keys(Keys.ESCAPE)
+    return True
 
-    #Friends check
+
+def validate_friends(driver):
     elem = driver.find_element_by_xpath("//a[@data-tab-key='friends']").find_element_by_tag_name("span")
     if int(elem.text) < MIN_FRIENDS:
         driver.close()
         return False
-
-    #Picture check
-
-    #Other checks?
-
-
-    driver.close()
-    return True
+    else:
+        return True
